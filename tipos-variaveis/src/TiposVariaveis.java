45b4e57c86c79002333793fc893bc17fd7376889
viniciuspedrassi <mviniciuscpedrassi@gmail.com>
2023-03-15 11:35:47 -0300

    tipos de variaveis e suas utilizações

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,32 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        /*Tabela de Tipos Primitivos e seus valores:
+
+byte  - 1 byte = -128 até 127
+short - 2 byte = -32.768 até 32.767
+int -   4 byte = -2.147.483.648 até 2.147.483.647
+long -  8 byte = -9,233,372,036,854,775.808 até 9.223.372.036.854.775.807
+
+float - 4 byte = -3,4028E + 38 até 3,4028E +38
+double - 8 byte = -1,7976E + 308 até 1,7976E + 308
+
+IMPORTANTE RESSALTAR: 
+OS MAIS COMUNS A SEREM UTILIZADOS HOJE EM DIA SÃO
+INT - PARA NUMEROS INTEIROS
+DOUBLE - PARA NUMEROS FRACIONADOS.
+FINAL -  SEMPRE EM CAIXA ALTA, PARA ESTABELECER UMA VARIAVEL QUE NÃO PODERÁ MUDAR SEU VALOR
+*/
+
+
+   final double VALOR_DE_PI = 3.14;
+
+    
+
+    System.out.print (VALOR_DE_PI);
+
     }
+
+
+  
 }
